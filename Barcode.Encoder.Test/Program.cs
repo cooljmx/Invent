34f81c6070c2f22c879aa1a857506d07c63131b7
cooljmx <cooljmx@gmail.com>
2014-04-07 19:29:39 +0400

    Barcode.Encoder.Test

---
+++ Barcode.Encoder.Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Barcode.Encoder.Test
+{
+    
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string crc = "01920";
+            const string id = "0000010";
+            const string barcode = crc + id;
+            Console.WriteLine(Decoder.Crc(barcode));
+            Console.WriteLine(Decoder.Id(barcode));
+            Console.WriteLine(Decoder.IsValidate(barcode) ? "Validate" : "Invalidate");
+            Console.WriteLine(Coder.Calculate(Convert.ToUInt32(id)));
+            Console.ReadKey();
+        }
+    }
+}
