02d53c941d3b8e6dcbcdf5b8503d2c73cb1a186f
lomovod <lomovod@gmail.com>
2014-04-09 11:01:45 +0400

    Принтер Brother PT2700VP

---
+++ Printer.BrotherРТ2700VP.Simple/StickPrinter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Barcode.Encoder;
+using Invent.Interfaces;
+using System.IO;
+using bpac;
+
+namespace Printer.BrotherРТ2700VP.Simple
+{
+    public class StickPrinter : Invent.Interfaces.IPrinter
+    {
+        public void Print(IRegisterDetail item)
+        {
+            string template = string.Concat(AppDomain.CurrentDomain.BaseDirectory, @"drivers\Simple.lbx");
+
+            string barcode = Coder.Calculate(Convert.ToUInt32(item.Id));
+            string name = item.DetailType.Id > 0 ? item.DetailType.Name : item.Name;
+
+            bpac.DocumentClass doc = new DocumentClass();
+            if (doc.Open(template))
+            {
+                doc.GetObject("Code").Text = barcode;
+                doc.GetObject("InvNum").Text = item.Register.InvNumber;
+                doc.GetObject("Name").Text = string.Format("{0}. {1}", item.Npp, name);
+
+                doc.StartPrint("",  PrintOptionConstants.bpoChainPrint | PrintOptionConstants.bpoAutoCut);
+                doc.PrintOut(1, PrintOptionConstants.bpoChainPrint | PrintOptionConstants.bpoAutoCut);
+                doc.EndPrint();
+                doc.Close();
+            }
+            else
+            {
+                throw new Exception("Невозможно открыть шаблон");
+            }
+        }
+    }
+}
